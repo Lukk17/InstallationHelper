3c4ae5973692f777d846c64d10630df300031e86
Łukasz Sarna <luksarna@gmail.com>
2022-10-15 19:37:28 +0200

    postgres configuration chrome install extensions update

---
+++ ubuntu/test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+echo "-------------"
+echo "| Testing.. |"
+echo "-------------"
+
+# examples
+# wget -P $temp_folder_path/ "$app_download_link"
+# tar -xf $temp_folder_path/app.tar.gz -C $temp_folder_path/
+# unzip -o $temp_folder_path/app.zip -d $temp_folder_path/appFolder/
+# gnome-extensions enable <UUID>
+
+temp_folder_path=~/.lukkInstall
+
+# =====================================================================================
+
