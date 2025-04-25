aecb247108a76e3c21081a3f3e05b112d04c25ac
Lukk <luksarna@gmail.com>
2025-04-25 17:40:16 +0200

    versions update, not essential app removal

--- Linux_live_setup/0.PrepareScripts.sh
+++ Linux_live_setup/0.PrepareScripts.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "Adding execute permisions to scripts.."
+echo "Adding execute permissions to scripts.."
 sudo chmod a+x ./1.InstallScript.sh
 sudo chmod a+x ./2.SystemConfig.sh
-sudo chmod a+x ./3.ConfigureEmulator.sh
-sudo chmod a+x ./updateApps.sh
