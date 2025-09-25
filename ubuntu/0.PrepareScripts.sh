c312d9a565b08a32cec2f4bf9923602df0e0a0e1
lukk <luksarna@gmail.com>
2025-09-25 23:12:04 +0200

    Refactor Ubuntu setup scripts: streamline software installation, replace deb package installs with Flatpak where possible, and remove deprecated software.

--- ubuntu/0.PrepareScripts.sh
+++ ubuntu/0.PrepareScripts.sh
@@ -4,4 +4,5 @@ echo "Adding execute permissions to scripts.."
 sudo chmod a+x ./1.UbuntuNewInstall.sh
 sudo chmod a+x ./2.UbuntuConfigure.sh
 sudo chmod a+x ./3.WaydroidInstall.sh
-sudo chmod a+x ./4.CleanUp.sh
+sudo chmod a+x ./4.WineInstall.sh
+sudo chmod a+x ./5.CleanUp.sh
